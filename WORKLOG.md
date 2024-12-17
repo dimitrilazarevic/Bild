9ab2e55d67199bbd17e9aad7fa07e24ee3805a42
dlazarevic <lazarevic.dimitri@gmail.com>
2024-12-17 21:07:35 +0100

    feat: can visualize a 360 picture, with a built-in zoom feature

--- WORKLOG.md
+++ WORKLOG.md
@@ -1,3 +1,17 @@
+# 17/12/2024
+- [x] : créer un threejs playground
+    - Abrogé. Dès maintenant, tout se passera ici, ça évitera de faire des aller retours.
+- [x] : Afficher ma première image
+- [ ] : Lister les fonctionnalités de base
+- [x] : Permettre de zoomer avec la molette
+    - Fait avec un event listener sur le canvas qui écoute wheel. On définit ensuite une position prédéfinie de zoom avec une vitesse de rotation différentiée.
+- [x] : Permettre de rotationner
+    - Grâce aux OrbitControls.
+- [ ] : Pouvoir placer des markers à des endroits avec un toggle
+- [ ] : Pouvoir associer à ces markers un chemin vers l'image suivante
+- [ ] : Pouvoir accéder à une autre image en cliquant sur ce marqueur
+
+
 # 12/12/2024
 
 Premier jour de Bild. Grande émotion.
@@ -6,7 +20,7 @@ Travail à faire :
 
 - [x] : Choisir la library pour afficher les images
     - Ce sera threejs.
-- [ ] : Apprendre des trucs de base sur threejs...
-- [ ] : créer un threejs playground
-- [ ] : Afficher ma première image
-- [ ] : Lister les fonctionnalités de base
+- [x] : Apprendre des trucs de base sur threejs...
+    - Essentiellement, 3js fonctionne avec un canvas dans lequel on va faire fonctionner un renderer. Ce renderer prend une scène, et une camera. La camera est quelque part, avec une orientation et un champ de vision. La scène a des mesh à partir de géométries et de matériaux qui peuvent eux mêmes être faits à partir de texture.
+    - Dans le cas de la vision 360, il suffit de créer une sphère sur laquelle on applique une projection equirectangulaire de deux images dites fisheye. La camera est au centre et ne fait que tourner. Le zoom est assuré par MAJ du FOV. 
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,96 @@
 <script>
-    import * as THREE from 'three'; 
 
+
+    import * as THREE from 'three';
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+
+        const canvas = document.querySelector("#canvas");
+        const loader = new THREE.TextureLoader();
+
+        if (!canvas) {
+            console.error("Canvas element not found!");
+            return;
+        }
+
+        const renderer = new THREE.WebGLRenderer({antialias:true,canvas});
+        renderer.setSize(window.innerWidth,window.innerHeight);
+
+        const availableFovs = [8,15,40,80];
+        const availableRotateSpeeds = [-0.3,-0.2,-0.1,-0.05];
+        let cameraZoomIndex = 1 ;
+        const camera = new THREE.PerspectiveCamera(availableFovs[cameraZoomIndex],window.innerWidth / window.innerHeight, 0.1, 5);
+
+        const controls = new OrbitControls(camera,canvas);
+
+        controls.enableZoom = false ;
+        controls.enablePan = false ;
+        camera.position.set(-0.1, 0, 0) ;
+        updateZoom(cameraZoomIndex);
+
+        canvas.addEventListener('wheel',(e)=>{
+
+            let wheelDirection = e.wheelDelta > 0 ? "zoom-in":"zoom-out";
+
+            switch (wheelDirection) {
+
+                case "zoom-in" :
+                    if(cameraZoomIndex < availableFovs.length-1){
+                        cameraZoomIndex++
+                    }
+                break;
+                case "zoom-out" :
+                    if(cameraZoomIndex >= 1){
+                        cameraZoomIndex--
+                    }
+                break;
+            }
+            
+            updateZoom(cameraZoomIndex);
+
+        })
+
+        const scene = new THREE.Scene();
+
+        const geometry = new THREE.SphereGeometry( 1, 30, 30 );
+
+        const texture = loader.load('/test_images/bedroom.jpg');
+
+        texture.wrapS = THREE.RepeatWrapping
+        texture.repeat.x = -1
+
+        const material = new THREE.MeshBasicMaterial({
+            map: texture,
+            side: THREE.DoubleSide
+        })
+
+        const sphere = new THREE.Mesh(geometry,material);
+
+        scene.add(sphere);
+
+        function animate() {
+
+            requestAnimationFrame( animate );
+
+            // required if controls.enableDamping or controls.autoRotate are set to true
+            controls.update();
+            renderer.render( scene, camera );
+
+        }
+
+        function updateZoom(i){
+            camera.setFocalLength(availableFovs[i]);
+            controls.rotateSpeed = availableRotateSpeeds[i] ;
+        }
+
+        animate();
+
+    });
+    
 </script>
 
 <main>
-    <div class="panorama_viewer"></div>
+    <canvas id="canvas"></canvas>
 </main>
